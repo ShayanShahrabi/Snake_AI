53deb07872b39b47df775c4b08c8b0715b1cd6ba
Shayan Shahrabi <shayanshahrab@gmail.com>
2023-07-12 20:26:34 -0700

    made small changes to codes and bug fixes, ready for main branch merge

--- model.py
+++ model.py
@@ -3,41 +3,43 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
-#--------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
 class Linear_QNet(nn.Module):
+    #--------------------------------------------------------------------------------------
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def forward(self, x):
-            x = F.relu(self.linear1(x))
-            x = self.linear2(x)
-            return x
-    #----------------------------------------------------------------
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+    #--------------------------------------------------------------------------------------
     def save(self, file_name='model.pth'):
         model_folder_path = './model'
         if not os.path.exists(model_folder_path):
-                    os.makedirs(model_folder_path)
-        file_name = os.path.join(model_folder_path, file_name) # model name for saving
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
         torch.save(self.state_dict(), file_name)
-    #----------------------------------------------------------------
-#--------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+
 class QTrainer:
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def __init__(self, model, lr, gamma):
         self.lr = lr
         self.gamma = gamma
         self.model = model
         self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
         self.criterion = nn.MSELoss()
-    #----------------------------------------------------------------
-    def train_step(self, state_old, final_move, reward, next_state, done):
+    #--------------------------------------------------------------------------------------
+    def train_step(self, state, action, reward, next_state, done):
         state = torch.tensor(state, dtype=torch.float)
         next_state = torch.tensor(next_state, dtype=torch.float)
         action = torch.tensor(action, dtype=torch.long)
         reward = torch.tensor(reward, dtype=torch.float)
-        #(n, x)
+        # (n, x)
 
         if len(state.shape) == 1:
             # (1, x)
@@ -56,8 +58,8 @@ class QTrainer:
             if not done[idx]:
                 Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
 
-            target[idx][torch.argmax(action).item()] = Q_new            
-
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+    
         # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
         # pred.clone()
         # preds[argmax(action)] = Q_new
@@ -66,5 +68,5 @@ class QTrainer:
         loss.backward()
 
         self.optimizer.step()
-    #----------------------------------------------------------------
-#--------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+
--- snake_game.py
+++ snake_game.py
@@ -1,26 +1,18 @@
-# a snake game played with arrow keys
 import pygame
 import random
 from enum import Enum
 from collections import namedtuple
 import numpy as np
-#----------------------------------------------------------------
+
 pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
 #font = pygame.font.SysFont('arial', 25)
-#----------------------------------------------------------------
-# reset function: after each game, the agent shall be able to reset the game
-# reward
-# play (action) -> direction
-# game iteration
-#is_collision
-#----------------------------------------------------------------
 class Direction(Enum):
     RIGHT = 1
     LEFT = 2
     UP = 3
     DOWN = 4
-#----------------------------------------------------------------
+
 Point = namedtuple('Point', 'x, y')
 
 # rgb colors
@@ -31,10 +23,10 @@ BLUE2 = (0, 100, 255)
 BLACK = (0,0,0)
 
 BLOCK_SIZE = 20
-SPEED = 20
-#----------------------------------------------------------------
+SPEED = 40
+
 class SnakeGameAI:
-    #initialize the game
+    #--------------------------------------------------------------------------------------
     def __init__(self, w=640, h=480):
         self.w = w
         self.h = h
@@ -43,29 +35,28 @@ class SnakeGameAI:
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
         self.reset()
-        
-        
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def reset(self):
         # init game state
         self.direction = Direction.RIGHT
-        
+
         self.head = Point(self.w/2, self.h/2)
-        self.snake = [self.head, Point(self.head.x-BLOCK_SIZE, self.head.y),Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
-        
+        self.snake = [self.head,
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+
         self.score = 0
         self.food = None
         self._place_food()
-
         self.frame_iteration = 0
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def _place_food(self):
-        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
+        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
         y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
         self.food = Point(x, y)
         if self.food in self.snake:
             self._place_food()
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def play_step(self, action):
         self.frame_iteration += 1
         # 1. collect user input
@@ -73,7 +64,7 @@ class SnakeGameAI:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
+        
         # 2. move
         self._move(action) # update the head
         self.snake.insert(0, self.head)
@@ -81,11 +72,11 @@ class SnakeGameAI:
         # 3. check if game over
         reward = 0
         game_over = False
-        if self.is_collision() or self.frame_iteration > 100*len(self.snake): # break in case of collision/doing nothing too long
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
             reward = -10
             return reward, game_over, self.score
-            
+
         # 4. place new food or just move
         if self.head == self.food:
             self.score += 1
@@ -97,35 +88,34 @@ class SnakeGameAI:
         # 5. update ui and clock
         self._update_ui()
         self.clock.tick(SPEED)
-
         # 6. return game over and score
         return reward, game_over, self.score
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def is_collision(self, pt=None):
         if pt is None:
             pt = self.head
         # hits boundary
-        if ptd.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
             return True
         # hits itself
-        if self.head in self.snake[1:]:
+        if pt in self.snake[1:]:
             return True
-        
+
         return False
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def _update_ui(self):
         self.display.fill(BLACK)
-        
+
         for pt in self.snake:
             pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
             pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
-            
+
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
-        
+
         text = font.render("Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
-    #----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
     def _move(self, action):
         # [straight, right, left]
 
@@ -136,12 +126,12 @@ class SnakeGameAI:
             new_dir = clock_wise[idx] # no change
         elif np.array_equal(action, [0, 1, 0]):
             next_idx = (idx + 1) % 4
-            new_dir = clock_wise[idx] # right turn r -> d -> l -> u
-        else: #[0, 0, 1]
+            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        else: # [0, 0, 1]
             next_idx = (idx - 1) % 4
-            new_dir = clock_wise[idx] # left turn r -> u -> l -> d
-        
-        self.direction  = new_dir
+            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+
+        self.direction = new_dir
 
         x = self.head.x
         y = self.head.y
@@ -153,6 +143,6 @@ class SnakeGameAI:
             y += BLOCK_SIZE
         elif self.direction == Direction.UP:
             y -= BLOCK_SIZE
-            
+
         self.head = Point(x, y)
-            
+    #--------------------------------------------------------------------------------------
